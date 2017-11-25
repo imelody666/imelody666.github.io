16dce403039d01d7058a09efb40198c6f8cff911
imelody666 <33961737+imelody666@users.noreply.github.com>
2017-11-26 00:55:02 +0800

    dsing

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#rectangle|#countdown 3||');
+      S.UI.simulate('happy|brithday|to|to start|#rectangle|#countdown 5||');
     }
 
     S.Drawing.loop(function () {
