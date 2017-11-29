1c429c4e1c8b03c12ca4c3922ea002673649eb63
imelody666 <33961737+imelody666@users.noreply.github.com>
2017-11-29 19:00:12 +0800

    112
    112

--- renxi/love.js
+++ renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "13px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("小佩，点这里o(*￣︶￣*)o", 28, 10);
+            ctx.fillText("小佩，点开心o(*￣︶￣*)o", 28, 10);
             ctx.restore();
         },
         clear: function() {
